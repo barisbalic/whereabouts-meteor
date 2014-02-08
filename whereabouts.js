027f5631b9ee36c9197953ee2dc1ece45fe57a8c
Baris Balic <baris.balic@uswitch.com>
2014-02-08 18:48:58 +0000

    Removing outdated cruft.

--- whereabouts.js
+++ whereabouts.js
@@ -4,22 +4,15 @@ if (Meteor.isClient) {
   Session.setDefault('mongoSearch', '');
   Session.setDefault('mongoSort', 'connected');
 
-  // Template.deviceDetails.unnamedDevice = function() {
-  //   console.log('Show new: ' + headers.get('x-forwarded-for'));
-  //   // Devices.find({})
-  //   // console.log(headers.get());
-  // };
-
   Template.deviceList.devices = function () {
     var filters = Session.get('mongoSearch');
     var mongoSearch = {username: new RegExp(filters, 'i')};
 
-    // var order = Session.get('mongoSort');
-    // var mongoSort = {};
-    // mongoSort[order] = -1;
+    var order = Session.get('mongoSort');
+    var mongoSort = {};
+    mongoSort[order] = -1;
 
-    // return Devices.find(mongoSearch, { sort: mongoSort });
-    return Devices.find(mongoSearch, { sort: {connected: -1, connectedAt: -1} });
+    return Devices.find(mongoSearch, { sort: mongoSort });
   };
 
   Template.deviceList.isBasement = function(location) {
